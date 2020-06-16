7f956130ae92523a26b0bda5a32ad13ba61ef593
jeffreza <jeffreza041992@gmail.com>
2020-06-16 08:50:56 +0500

    done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 
 app.get("/", (req, res) => {
-  res.send("hello get");
+  res.render('index.html');
 });
 
 app.post("/", (req, res) => {
-  res.send("hello post");
+  res.render('events.html');
 });
 
 app.listen(3000, (req,res)=>{
